18d09f4789e2ae0ae478c510ebec9992b4461f71
Anderson <30065845+azzevedo@users.noreply.github.com>
2022-06-13 17:41:02 -0300

    Create sorting.py

---
+++ sorting.py
@@ -0,0 +1,15 @@
+def sorting(data):
+	for k in range(len(data) - 1):
+		min_idx = k
+		for i in range(k+1, len(data)):
+			if data[i] < data[min_idx]:
+				min_idx = i
+		#data[k], data[min_idx] = data[min_idx], data[k]
+		aux = data[k]
+		data[k] = data[min_idx]
+		data[min_idx] = aux
+	return data
+
+
+data = [10, 2, 5, 1, 7, 823, 23, 6]
+print(sorting(data))
